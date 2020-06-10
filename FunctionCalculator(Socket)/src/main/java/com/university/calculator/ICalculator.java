8d3bf1c1d4ebd207716889c23a96ad31115230c3
StasDzundza <stas.dzundza@gmail.com>
2020-06-10 17:42:57 +0300

    delete extending remote from ICalculator

--- FunctionCalculator(Socket)/src/main/java/com/university/calculator/ICalculator.java
+++ FunctionCalculator(Socket)/src/main/java/com/university/calculator/ICalculator.java
@@ -3,6 +3,6 @@ package com.university.calculator;
 import java.rmi.Remote;
 import java.util.List;
 
-public interface ICalculator extends Remote {
+public interface ICalculator {
     List<Double> calculate(double a, double xFrom, double xTo, double step);
 }
